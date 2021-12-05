283f37dadd8d1e9cdc6d04cd8552e5e758f55ad9
MR.THANUWA <93111735+thanuwasmith@users.noreply.github.com>
2021-12-05 13:34:54 +0530

    first commit

---
+++ js/templatemo-script.js
@@ -0,0 +1,19 @@
+/* code by MR_THANUWA MOD*/
+$(function() {
+    $(".navbar-toggler").on("click", function(e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+      });
+    
+      $("html").click(function(e) {
+        var header = document.getElementById("tm-header");
+    
+        if (!header.contains(e.target)) {
+          $(".tm-header").removeClass("show");
+        }
+      });
+    
+      $("#tm-nav .nav-link").click(function(e) {
+        $(".tm-header").removeClass("show");
+      });
+});
